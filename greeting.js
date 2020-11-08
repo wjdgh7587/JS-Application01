a585505ffcf56311da5c962cf056340bf5d16d4a
Jeongho Y <31297335+wjdgh7587@users.noreply.github.com>
2020-11-09 01:43:32 +0900

    Update naming function

---
+++ greeting.js
@@ -0,0 +1,49 @@
+const form = document.querySelector(".js-form"),
+    input = form.querySelector("input"),
+    greeting = document.querySelector(".js-greetings");
+console.log(form);
+console.log(input);
+
+const USER_LS = "currentUser",
+    SHOWING_CN = "showing";
+
+function saveName(text) {
+    localStorage.setItem(USER_LS, text);
+}
+
+function handleSubmit(event) {
+    //prevent default
+    event.preventDefault();
+    //기본적으로 submit을 실행하면 사라지게 되어있는데 이를 방지 가능(이벤트 금지)
+    const currentValue = input.value;
+    //console.log(currentValue);
+    paintGreeting(currentValue);
+    saveName(currentValue);
+}
+
+function paintGreeting(text) {
+    form.classList.remove(SHOWING_CN);
+    greeting.classList.add(SHOWING_CN);
+    greeting.innerText = `Hello ${text}`;
+}
+
+function askForName() {
+    form.classList.add(SHOWING_CN);
+    form.addEventListener("submit", handleSubmit);
+}
+
+function loadName() {
+    const currentUser = localStorage.getItem(USER_LS);
+    if (currentUser === null) {
+        askForName();
+    } else {
+        paintGreeting(currentUser);
+    }
+}
+
+function init() {
+    loadName();
+}
+init();
+
+//localStorage.setItem("key_name", value);
