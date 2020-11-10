fb1315b82ce3071adf791bcf6d3f49725610beac
Jeongho Y <31297335+wjdgh7587@users.noreply.github.com>
2020-11-10 14:34:23 +0900

    update edit
    I have to find some of information to edit list tag

--- todo.js
+++ todo.js
@@ -1,6 +1,6 @@
 const toDoForm = document.querySelector(".js-toDoForm"),
-    toDoInput = toDoForm.querySelector("input"),
-    toDoList = document.querySelector(".js-toDoList");
+  toDoInput = toDoForm.querySelector("input"),
+  toDoList = document.querySelector(".js-toDoList");
 
 // console.log(toDoForm);
 // console.log(input);
@@ -9,103 +9,107 @@ const toDoForm = document.querySelector(".js-toDoForm"),
 var TODO_LS = "toDos";
 var TODO_ARRAY = [];
 
-//function of filter
-// function filterFunction(toDoList) {
-//     return toDoList.id === 1;
-// }
+//function of edit
 
-//function of commit
+function editToDo(event) {
+  const editBtnList = event.target.parentNode;
+  const currentValue = editBtnList.childNodes[2].textContent;
+  const getIdValue = editBtnList.id;
 
-function commitToDo(event) {
-    const commitBtnList = event.target.parentNode;
-    const currentValue = commitBtnList.childNodes[2];
+  const listItem = this.parentNode;
+  const listText = listItem.querySelector("span");
 
-    const toDoCommit = ;
-    console.log(commitBtnList);
-    console.log(currentValue);
+  //const span = document.querySelector("span").textContent;
+  const checkingTag = listItem.innerText;
+  //innerText 자식의 모든 텍스트 표시
+
+  toDoInput.value = currentValue;
+
+  console.log(checkingTag);
 }
 
 //function of delete
 
 function deleteToDo(event) {
-    const delBtnList = event.target.parentNode;
-    toDoList.removeChild(delBtnList);
-    const toDoClean = TODO_ARRAY.filter(function(toDoList) {
-        //console.log(delBtnList.id, toDoList.id);
+  const delBtnList = event.target.parentNode;
+  toDoList.removeChild(delBtnList);
+  const toDoClean = TODO_ARRAY.filter(function (toDoList) {
+    //console.log(delBtnList.id, toDoList.id);
 
-        return toDoList.id !== parseInt(delBtnList.id);
-    });
-    TODO_ARRAY = toDoClean;
-    //console.log(toDoClear);
-    saveToDos();
+    return toDoList.id !== parseInt(delBtnList.id);
+  });
+  TODO_ARRAY = toDoClean;
+  //console.log(toDoClear);
+  saveToDos();
 }
 
 //function of save toDoList
 function saveToDos() {
-    localStorage.setItem(TODO_LS, JSON.stringify(TODO_ARRAY));
-    //What is Json??
+  localStorage.setItem(TODO_LS, JSON.stringify(TODO_ARRAY));
+  //What is Json??
 }
 
 //Paint to the Screen
 function paintToDo(text) {
-    //console.log(text);
-    const li = document.createElement("li");
-    const commitBtn = document.createElement("button");
-    const delBtn = document.createElement("button");
-    commitBtn.addEventListener("click", commitToDo);
-    delBtn.addEventListener("click", deleteToDo);
-    var toDoId = TODO_ARRAY.length + 1;
+  //console.log(text);
+  const li = document.createElement("li");
+  const editBtn = document.createElement("button");
+  const delBtn = document.createElement("button");
+  editBtn.addEventListener("click", editToDo);
+  delBtn.addEventListener("click", deleteToDo);
+  var toDoId = TODO_ARRAY.length + 1;
 
-    commitBtn.innerHTML = "✏️";
-    delBtn.innerHTML = "❎";
-    const span = document.createElement("span");
-    span.innerText = text + " ";
+  editBtn.innerHTML = "✏️";
+  delBtn.innerHTML = "❎";
+  const span = document.createElement("span");
+  span.innerText = text;
 
-    //input list to the HTML page
-    li.appendChild(commitBtn);
-    li.appendChild(delBtn);
-    li.appendChild(span);
-    li.id = toDoId;
+  //input list to the HTML page
+  li.appendChild(editBtn);
+  li.appendChild(delBtn);
+  li.appendChild(span);
+  li.id = toDoId;
 
-    toDoList.appendChild(li);
+  toDoList.appendChild(li);
 
-    const toDoObj = {
-        text: text,
-        id: toDoId,
-    };
+  const toDoObj = {
+    text: text,
+    id: toDoId,
+  };
 
-    TODO_ARRAY.push(toDoObj);
-    saveToDos();
+  TODO_ARRAY.push(toDoObj);
+  saveToDos();
 
-    // console.log(TODO_ARRAY);
+  // console.log(TODO_ARRAY);
 }
 
 //Form action
 function handleSubmit(event) {
-    event.preventDefault();
-    const currentValue = toDoInput.value;
-    paintToDo(currentValue);
-    toDoInput.value = "";
-    //console.log(currentValue);
+  event.preventDefault();
+  const currentValue = toDoInput.value;
+  paintToDo(currentValue);
+  toDoInput.value = "";
+  //console.log(currentValue);
 }
 
 //checking local storage
 function loadToDos() {
-    const loadToDosValue = localStorage.getItem(TODO_LS);
-    //Always showing
-    if (loadToDosValue !== null) {
-        var parseToDosValue = JSON.parse(loadToDosValue);
-        //console.log(parseToDosValue);
-        parseToDosValue.forEach(function(toDo) {
-            //console.log(toDo.text);
-            paintToDo(toDo.text);
-        });
-    } else {}
+  const loadToDosValue = localStorage.getItem(TODO_LS);
+  //Always showing
+  if (loadToDosValue !== null) {
+    var parseToDosValue = JSON.parse(loadToDosValue);
+    //console.log(parseToDosValue);
+    parseToDosValue.forEach(function (toDo) {
+      //console.log(toDo.text);
+      paintToDo(toDo.text);
+    });
+  } else {
+  }
 }
 
 function init() {
-    loadToDos();
-    toDoForm.addEventListener("submit", handleSubmit);
+  loadToDos();
+  toDoForm.addEventListener("submit", handleSubmit);
 }
 
 init();
